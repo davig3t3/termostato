8bd907a81862dcb63ca970fff15cefd3f84eafa5
davig3t3 <davig3t3@gmail.com>
2023-05-02 02:08:00 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -2,10 +2,17 @@ import time
 inicio = time.time()
 
 # Código a medir
+# The code defines a lambda function `R` that takes no arguments and returns a map object that applies
+# the `int` function to each element of the input obtained from the `input()` function after splitting
+# it by whitespace.
 R = lambda: map(int, input().split())
 
 t, = R()
 
+# The code is running a loop `t` times, where `t` is the first input obtained from the user. In each
+# iteration of the loop, the code reads three integers `l`, `r`, and `x` from the user using the `R`
+# lambda function. Then, it reads two more integers `a` and `b` using the `R` function and sorts them
+# in ascending order.
 for _ in range(t):
     l, r, x = R()
     a, b = sorted(R())
